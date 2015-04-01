ebc80522aa2bb44eccc3bb39d40ce7a2c4c2c3c4
BenjamenMeyer <ben.meyer@rackspace.com>
2015-04-01 19:15:52 +0000

    Enhancement: Update user by user id

--- openstackinabox/services/keystone/v2.py
+++ openstackinabox/services/keystone/v2.py
@@ -12,10 +12,32 @@ from openstackinabox.models.keystone import KeystoneModel
 from openstackinabox.services.base_service import BaseService
 
 
+class KeystoneV2Errors(Exception):
+    pass
+
+
+class KeystoneV2AuthError(Exception):
+    pass
+
+
+class KeystoneV2AuthForbiddenError(KeystoneV2AuthError):
+    pass
+
+
+class KeystoneV2AuthUnauthorizedError(KeystoneV2AuthError):
+    pass
+
+
 class KeystoneV2Service(BaseService):
 
     # USER_ID_PATH_REGEX = re.compile('^\/users\/[a-zA-Z]+[\w\.@-]*$')
     USER_ID_PATH_REGEX = re.compile('^\/users\/([0-9]+)$')
+
+    @staticmethod
+    def get_user_id_from_path(uri_path):
+        uri_matcher =  KeystoneV2Service.USER_ID_PATH_REGEX.match(uri_path)
+        userid =  uri_matcher.groups()[0]
+        return userid
 
     def __init__(self):
         super(KeystoneV2Service, self).__init__('keystone/v2.0')
@@ -34,6 +56,9 @@ class KeystoneV2Service(BaseService):
         self.register(BaseService.GET,
                       KeystoneV2Service.USER_ID_PATH_REGEX,
                       KeystoneV2Service.handle_get_user_by_id)
+        self.register(BaseService.POST,
+                      KeystoneV2Service.USER_ID_PATH_REGEX,
+                      KeystoneV2Service.handle_update_user_by_id)
         self.log_info('initialized')
 
     @property
@@ -47,8 +72,52 @@ class KeystoneV2Service(BaseService):
         else:
             raise TypeError('model is not an instance of KeystoneModel')
 
+    def helper_validate_token(self, request_headers, enforce_admin, service_admin):
+        if 'x-auth-token' not in request_headers:
+            raise KeystoneV2AuthForbiddenError('no auth token')
+
+
+        try:
+            auth_token = request_headers['x-auth-token']
+            user_data = None
+            if service_admin:
+                user_data = self.model.validate_token_service_admin(auth_token)
+
+            elif enforce_admin:
+                user_data = self.model.validate_token_admin(auth_token)
+
+            else:
+                user_data = self.model.validate_token(auth_token)
+
+            self.log_debug('token {0} maps to tenant {1} and userid {2}'
+                           .format(auth_token,
+                                   user_data['tenantid'],
+                                   user_data['userid']))
+        except Exception as ex:
+            raise KeystoneV2AuthUnauthorizedError(
+                'invalid or expired auth token')
+
+        return user_data
+
+    def helper_authenticate(self, request_headers, headers, enforce_admin, service_admin):
+
+        try:
+            user_data = self.helper_validate_token(request_headers,
+                                                   enforce_admin,
+                                                   service_admin)
+
+        except KeystoneV2AuthForbiddenError:
+            self.log_exception('no token')
+            return (403, headers, 'Forbidden')
+
+        except KeystoneV2AuthUnauthorizedError:
+            self.log_exception('invalid or expired token')
+            return (401, headers, 'Not Authorized')
+
+        return user_data 
+
+
     def handle_list_tenants(self, request, uri, headers):
-        req_headers = request.headers
         '''
         200, 203 -> OK
         400 -> Bad Request: one or more required parameters
@@ -61,38 +130,39 @@ class KeystoneV2Service(BaseService):
         503 -> Service Fault
         '''
         self.log_request(uri, request)
+        req_headers = request.headers
 
-        if 'x-auth-token' in req_headers:
-            if req_headers['x-auth-token'] == self.model.get_admin_token():
-                """
-                Body on success:
-                body = {
-                    'tenants' : [ {'id': 01234,
-                                   'name': 'bob',
-                                   'description': 'joe bob',
-                                   'enabled': True }]
-                    'tenants_links': []
-                }
-                """
-                response_body = {
-                    'tenants': [tenant_info
-                                for tenant_info in
-                                self.model.get_tenants()],
-                    'tenants_links': []
-                }
-                return (200, headers, json.dumps(response_body))
+        user_data = self.helper_authenticate(req_headers, headers, True, True)
+        if isinstance(user_data, tuple):
+            return user_data
 
-            else:
-                return (401, headers, 'Not Authorized')
-
-        else:
-            return (403, headers, 'Forbidden')
+        """
+        Body on success:
+        body = {
+            'tenants' : [ {'id': 01234,
+                           'name': 'bob',
+                           'description': 'joe bob',
+                           'enabled': True }]
+            'tenants_links': []
+        }
+        """
+        response_body = {
+            'tenants': [tenant_info
+                        for tenant_info in
+                        self.model.get_tenants()],
+            'tenants_links': []
+        }
+        return (200, headers, json.dumps(response_body))
 
     def handle_list_users(self, request, uri, headers):
         req_headers = request.headers
         '''
         '''
         self.log_request(uri, request)
+
+        user_data = self.helper_authenticate(req_headers, headers, True, False)
+        if isinstance(user_data, tuple):
+            return user_data
 
         def user_data_filter(user):
             self.log_debug('Filtering data on {0}'.format(user))
@@ -103,43 +173,29 @@ class KeystoneV2Service(BaseService):
                 'email': user['email'],
             }
 
-        if 'x-auth-token' in req_headers:
-            try:
-                user_data = self.model.validate_token_admin(
-                    req_headers['x-auth-token'])
+        parsed_uri = parse.urlparse(uri)
+        query = parsed_uri.query
 
-                self.log_debug('Token Valid for tenantid {0}'
-                               .format(user_data['tenantid']))
-
-                parsed_uri = parse.urlparse(uri)
-                query = parsed_uri.query
-
-                if len(query) > 0:
-                    query_data = parse.parse_qs(query)
+        if len(query) > 0:
+            query_data = parse.parse_qs(query)
 
 
-                    if 'name' in query_data:
-                        user_info = self.model.get_user_by_name(
-                            user_data['tenantid'],
-                            query_data['name'][0])
-                        response_body = {
-                            'user': user_data_filter(user_info)
-                        }
-                        return (200, headers, json.dumps(response_body))
-
+            if 'name' in query_data:
+                user_info = self.model.get_user_by_name(
+                    user_data['tenantid'],
+                    query_data['name'][0])
                 response_body = {
-                    'users': [user_data_filter(user_info)
-                              for user_info in
-                              self.model.get_users_for_tenant_id(
-                                  user_data['tenantid'])]
+                    'user': user_data_filter(user_info)
                 }
                 return (200, headers, json.dumps(response_body))
 
-            except Exception as ex:
-                self.log_exception('User List Failure')
-                return (401, headers, 'Not Authorized')
-        else:
-            return (403, headers, 'Forbidden')
+        response_body = {
+            'users': [user_data_filter(user_info)
+                      for user_info in
+                      self.model.get_users_for_tenant_id(
+                          user_data['tenantid'])]
+        }
+        return (200, headers, json.dumps(response_body))
 
     def handle_add_user(self, request, uri, headers):
         '''
@@ -196,75 +252,56 @@ class KeystoneV2Service(BaseService):
         self.log_request(uri, request)
         req_headers = request.headers
 
-        if 'x-auth-token' in req_headers:
-            try:
-                auth_token = req_headers['x-auth-token']
+        user_data = self.helper_authenticate(req_headers, headers, True, False)
+        if isinstance(user_data, tuple):
+            return user_data
 
-                user_data = self.model.validate_token(auth_token)
+        current_user = self.model.get_user_by_id(user_data['tenantid'],
+                                                 user_data['userid'])
 
-                self.log_debug('token {0} maps to tenant {1} and userid {2}'
-                               .format(auth_token,
-                                       user_data['tenantid'],
-                                       user_data['userid']))
+        req_body = request.body.decode('utf-8')
+        json_data = json.loads(req_body)
 
-            except Exception as ex:
-                self.log_exception('bad token lookup')
-                return (401, headers, 'Not Authorized')
+        try:
+            username = json_data['user']['username']
+            email = json_data['user']['email']
+            enabled = json_data['user']['enabled']
 
-            current_user = self.model.get_user_by_id(user_data['tenantid'],
-                                                     user_data['userid'])
+        except LookupError:
+            self.log_exception('missing parameter')
+            return (400, headers, 'bad request')
 
-            req_body = request.body.decode('utf-8')
-            json_data = json.loads(req_body)
+        if username == current_user['username']:
+            return (409, headers, 'user already exists')
 
-            try:
-                username = json_data['user']['username']
-                email = json_data['user']['email']
-                enabled = json_data['user']['enabled']
+        if not self.model.validate_username(username):
+            self.log_debug('username invalid')
+            return (400, headers, 'bad request')
 
-            except LookupError:
-                self.log_exception('missing parameter')
+        try:
+            password = json_data['user']['OS-KSADM:password']
+        except LookupError:
+            password = None
+       
+        if password is not None:
+            if not self.model.validate_password(password):
+                self.log_debug('invalid password')
                 return (400, headers, 'bad request')
 
-            if username == current_user['username']:
-                return (409, headers, 'user already exists')
+        try:
+            user_info = self.model.add_user(
+                tenantid=current_user['tenantid'],
+                username=username,
+                email=email,
+                password=password,
+                enabled=enabled)
 
-            if not self.model.validate_username(username):
-                self.log_debug('username invalid')
-                return (400, headers, 'bad request')
+            return (201, headers, 'ok')
 
-            try:
-                password = json_data['user']['OS-KSADM:password']
-            except LookupError:
-                password = None
-           
-            if password is not None:
-                if not self.model.validate_password(password):
-                    self.log_debug('invalid password')
-                    return (400, headers, 'bad request')
-
-            try:
-                user_info = self.model.add_user(
-                    tenantid=current_user['tenantid'],
-                    username=username,
-                    email=email,
-                    password=password,
-                    enabled=enabled)
-
-                return (201, headers, 'ok')
-
-            except Exception as ex:
-                self.log_exception('User Add Failure')
-                # is 404 correct?
-                return (404, headers, 'failed to add user')
-        else:
-            return (403, headers, 'Forbidden')
-
-    @staticmethod
-    def get_user_id_from_path(uri_path):
-        uri_matcher =  KeystoneV2Service.USER_ID_PATH_REGEX.match(uri_path)
-        userid =  uri_matcher.groups()[0]
-        return userid
+        except Exception as ex:
+            self.log_exception('User Add Failure')
+            # is 404 correct?
+            return (404, headers, 'failed to add user')
 
     def handle_get_user_by_id(self, request, uri, headers):
         '''
@@ -300,47 +337,103 @@ class KeystoneV2Service(BaseService):
         self.log_request(uri, request)
         req_headers = request.headers
 
+        user_data = self.helper_authenticate(req_headers, headers, False, False)
+        if isinstance(user_data, tuple):
+            return user_data
+
         try:
             user_id = KeystoneV2Service.get_user_id_from_path(uri)
             self.log_debug('Lookup of user id {0} requested'
                            .format(user_id))
+
         except Exception as ex:  #  pragma: no cover
             self.log_exception('Failed to get user id from path')
             return (400, headers, 'bad request')
 
-        if 'x-auth-token' in req_headers:
-            try:
-                auth_token = req_headers['x-auth-token']
-                user_data = self.model.validate_token(auth_token)
+        try:
+            user_info = self.model.get_user_by_id(user_data['tenantid'],
+                                                  user_id)
 
-                self.log_debug('token {0} maps to tenant {1} and userid {2}'
-                               .format(auth_token,
-                                       user_data['tenantid'],
-                                       user_data['userid']))
-            except Exception as ex:
-                self.log_exception('bad token lookup')
-                return (401, headers, 'Not Authorized')
+        except:
+            self.log_exception('failed to lookup user id {1} under tenant '
+                               'id {0}'
+                               .format(user_data['tenantid'], user_id))
+            return (404, headers, 'Not Found')
 
-            try:
-                user_info = self.model.get_user_by_id(user_data['tenantid'],
-                                                      user_id)
-
-            except:
-                self.log_exception('failed to lookup user id {1} under tenant '
-                                   'id {0}'
-                                   .format(user_data['tenantid'], user_id))
-                return (404, headers, 'Not Found')
-
-            data = {
-                'user': {
-                    'id': user_info['userid'],
-                    'name': user_info['username'],
-                    'email': user_info['email'],
-                    'enabled': user_info['enabled']
-                }
+        data = {
+            'user': {
+                'id': user_info['userid'],
+                'name': user_info['username'],
+                'email': user_info['email'],
+                'enabled': user_info['enabled']
             }
-            json_data = json.dumps(data)
-            return (200, headers, json_data)
+        }
+        json_data = json.dumps(data)
+        return (200, headers, json_data)
 
-        else:
-            return (403, headers, 'Forbidden')
+    def handle_update_user_by_id(self, request, uri, headers):
+        '''
+        200 -> OK
+        400 -> Bad Request
+        401 -> Unauthorized
+        403 -> Forbidden
+        404 -> Not Found
+        405 -> Invalid Method
+        413 -> Over Limit
+        415 -> Bad Media Type
+        503 -> Server Fault
+        '''
+        self.log_request(uri, request)
+        req_headers = request.headers
+
+        user_data = self.helper_authenticate(req_headers, headers, False, False)
+        if isinstance(user_data, tuple):
+            return user_data
+
+        try:
+            user_id = KeystoneV2Service.get_user_id_from_path(uri)
+            self.log_debug('Lookup of user id {0} requested'
+                           .format(user_id))
+
+        except Exception as ex:  #  pragma: no cover
+            self.log_exception('Failed to get user id from path')
+            return (400, headers, 'bad request')
+
+        req_body = request.body.decode('utf-8')
+        json_data = json.loads(req_body)
+
+        if 'user' not in json_data:
+            return (400, headers, 'bad request')
+
+        if 'id' not in json_data['user'] or\
+                'username' not in json_data['user']:
+            return (400, headers, 'bad request')
+
+        try:
+            user_info = self.model.get_user_by_id(user_data['tenantid'],
+                                                  user_id)
+        except Exception as ex:
+            self.log_exception('failed to get user data')
+            return (404, headers, 'Not Found')
+
+        if 'enabled' in json_data['user']:
+            user_info['enabled'] = json_data['user']['enabled']
+
+        if 'email' in json_data['user']:
+            user_info['email'] = json_data['user']['email']
+
+        if 'OS-KSADM:password' in json_data['user']:
+            user_info['password'] = json_data['user']['OS-KSADM:password']
+
+        try:
+            self.model.update_user_by_user_id(tenantid=user_data['tenantid'],
+                                              userid=user_id,
+                                              email=user_info['email'],
+                                              password=user_info['password'],
+                                              apikey=user_info['apikey'],
+                                              enabled=user_info['enabled'])
+        except Exception as ex:  # pragma: no cover
+            self.log_exception('failed to update user')
+            return (503, headers, 'Server error')
+
+        return (200, headers, json.dumps(user_info))
---
+++ openstackinabox/tests/test_keystone_v2_user_update.py
@@ -0,0 +1,172 @@
+"""
+Stack-In-A-Box: Basic Test
+"""
+import json
+import unittest
+
+import httpretty
+import mock
+import requests
+import stackinabox.util_httpretty
+from stackinabox.stack import StackInABox
+
+from openstackinabox.models.keystone.model import KeystoneModel
+from openstackinabox.services.keystone import KeystoneV2Service
+
+
+@httpretty.activate
+class TestKeystoneV2UserUpdate(unittest.TestCase):
+
+    def setUp(self):
+        super(TestKeystoneV2UserUpdate, self).setUp()
+        self.keystone = KeystoneV2Service()
+        self.headers = {
+            'x-auth-token': self.keystone.model.get_admin_token()
+        }
+        self.tenant_id = self.keystone.model.add_tenant(tenantname='neo',
+                                                        description='The One')
+        self.user_info = {
+            'user': {
+                'username': 'trinity',
+                'enabled': True,
+                'email': 'trinity@theone.matrix',
+                'OS-KSADM:password': 'Inl0veWithNeo'
+            }
+        }
+        self.user_info['user']['userid'] =\
+            self.keystone.model.add_user(tenantid=self.tenant_id,
+                                         username=self.user_info['user']['username'],
+                                         email=self.user_info['user']['email'],
+                                         password=
+                                         self.user_info['user']['OS-KSADM:password'],
+                                         enabled=self.user_info['user']['enabled'])
+        self.keystone.model.add_token(self.tenant_id,
+                                      self.user_info['user']['userid'])
+        StackInABox.register_service(self.keystone)
+
+    def tearDown(self):
+        super(TestKeystoneV2UserUpdate, self).tearDown()
+        StackInABox.reset_services()
+
+    def test_user_update_no_token(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            data=json_data)
+        self.assertEqual(res.status_code, 403)
+
+    def test_user_update_invalid_token(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        self.headers['x-auth-token'] = 'new_token' 
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json_data)
+        self.assertEqual(res.status_code, 401)
+
+    def test_user_update_no_user(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps({'family': {}}))
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_update_no_user_id(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps({'user': {}}))
+        self.assertEqual(res.status_code, 400)
+
+    def test_user_update_invalid_user_id(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        self.user_info['user']['userid'] = '1234567890'
+        self.user_info['user']['email'] = 'trinity@lost.matrix'
+        self.user_info['user']['id'] = self.user_info['user']['userid']
+        self.user_info['user']['enabled'] = False
+        self.user_info['user']['OS-KSADM:password'] = 'neocortex'
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps(self.user_info))
+        self.assertEqual(res.status_code, 404)
+
+    def test_user_update(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        self.user_info['user']['email'] = 'trinity@lost.matrix'
+        self.user_info['user']['id'] = self.user_info['user']['userid']
+        self.user_info['user']['enabled'] = False
+        self.user_info['user']['OS-KSADM:password'] = 'neocortex'
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps(self.user_info))
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_update_no_enabled(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        self.user_info['user']['email'] = 'trinity@lost.matrix'
+        self.user_info['user']['id'] = self.user_info['user']['userid']
+        del self.user_info['user']['enabled']
+        self.user_info['user']['OS-KSADM:password'] = 'neocortex'
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps(self.user_info))
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_update_no_email(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        del self.user_info['user']['email']
+        self.user_info['user']['id'] = self.user_info['user']['userid']
+        self.user_info['user']['enabled'] = False
+        self.user_info['user']['OS-KSADM:password'] = 'neocortex'
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps(self.user_info))
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_update_no_password(self):
+        stackinabox.util_httpretty.httpretty_registration('localhost')
+        json_data = json.dumps(self.user_info)
+        user_data = self.keystone.model.get_token_by_userid(
+            self.user_info['user']['userid'])
+        self.headers['x-auth-token'] = user_data['token']
+        self.user_info['user']['email'] = 'trinity@lost.matrix'
+        self.user_info['user']['id'] = self.user_info['user']['userid']
+        self.user_info['user']['enabled'] = False
+        del self.user_info['user']['OS-KSADM:password']
+        res = requests.post('http://localhost/keystone/v2.0/users/{0}'
+                            .format(self.user_info['user']['userid']),
+                            headers=self.headers,
+                            data=json.dumps(self.user_info))
+        self.assertEqual(res.status_code, 200)
